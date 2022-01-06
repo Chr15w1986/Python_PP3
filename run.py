fc961f1c60ae4f819663dc142706c2bdb262ebdf
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-01-06 21:31:09 +0000

    define function for random number

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
 """
-'NUMBER X', A numbers guessing game played versus the computer.
+Main game file for 'Number X'
 """
+import random
+
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f'Make a guess at a number between 1 and {x}: '))
+        print(guess)
+
+
+guess(10)
