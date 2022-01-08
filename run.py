2c27d24a357f9f73f0f5cf909a08316f957f52bc
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-01-08 09:15:41 +0000

    create if else statement for guess number

--- run.py
+++ run.py
@@ -4,12 +4,21 @@ Main game file for 'Number X'
 import random
 
 
-def guess(x):
-    random_number = random.randint(1, x)
+def guess(num):
+    """
+    Function to generate a random number between 1 and 10
+    """
+    random_number = random.randint(1, num)
     guess = 0
     while guess != random_number:
-        guess = int(input(f'Make a guess at a number between 1 and {x}: '))
+        guess = int(input(f'Make a guess at a number between 1 and {num}: '))
         print(guess)
+        if guess < random_number:
+            print('Oops, Guess a little higher!\n')
+        elif guess > random_number:
+            print('Hmmm, Guess a little lower!\n')
+
+    print(f'Well done! You guessed the correct number {random_number}\n')
 
 
 guess(10)
